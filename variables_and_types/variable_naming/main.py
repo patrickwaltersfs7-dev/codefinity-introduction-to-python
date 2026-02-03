e007aa78f92661c0d0311f7295e70e12777dfe54
patrickwaltersfs7-dev <>
2026-02-03 22:53:49 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,10 @@
+
+
+
+item_name=("Bread")
+print("Item:",item_name)
+item_price=(4.52)
+print("Price per loaf: $", item_price)
+items_in_stock=(230)
+print("Quantity in stock:", items_in_stock)
+print()
